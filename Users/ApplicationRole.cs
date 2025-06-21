a817c592b988f755b4f5f85d43ac936a1f90e6e4
pawel200020 <pawel200020@gmail.com>
2025-06-22 01:22:30 +0200

    user roles init

---
+++ Users/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Users;
+
+public class ApplicationRole : IdentityRole
+{
+    
+}
---
+++ Users/UserRole.cs
@@ -0,0 +1,11 @@
+﻿namespace Users;
+/// <summary>
+/// This enum encapsulates roles stored in database every edit needs a migration
+/// </summary>
+public enum UserRole
+{
+    Administrator,
+    Manager,
+    Standard,
+    Basic
+};
